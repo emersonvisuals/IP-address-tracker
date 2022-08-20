8cd8dd4e33b70459f9211e5d8282f16d0cc33aa3
Thomas Emerson <emersonvisuals@gmail.com>
2022-08-20 19:30:50 +1000

    API implementation WIP

--- src/app.js
+++ src/app.js
@@ -3,12 +3,30 @@ let ipAddressText = document.querySelector('h1.ipAddressValue');
 let locationText = document.querySelector('h1.locationValue');
 let timezoneText = document.querySelector('h1.timezoneValue');
 let ispText = document.querySelector('h1.ispValue');
+let userInput = document.querySelector('input.inputContainer');
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('Location')
+    .openPopup();
 
 
 button.addEventListener('click', () => {
 
+    function getVal(){
+        userInput.value;
+        console.log(userInput.value);  
+    }
+    
+    getVal();
+
     function getData() {
-        fetch('https://geo.ipify.org/api/v2/country?apiKey=at_VWo7RHLulsmKoMBYuuIVzf8DcAWYK&ipAddress=8.8.8.8')
+        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_VWo7RHLulsmKoMBYuuIVzf8DcAWYK&ipAddress=${userInput.value}`)
         .then(response => response.json())
         .then(data => {
             ipAddressText.innerHTML = data.ip;
@@ -16,6 +34,8 @@ button.addEventListener('click', () => {
             timezoneText.innerHTML = `${data.location.timezone}`;
             ispText.innerHTML = `${data.isp}`;
             console.log(data);
+            map.setView([`${data.location.lat}`, `${data.location.lng}`], `${data.location.geonameId}`);
+            L.marker([`${data.location.lat}`, `${data.location.lng}`]).addTo(map)
         })
     }
     

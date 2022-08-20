c2f3ef984cb31a32eeb1801fcf8bb05bbc20b60c
Thomas Emerson <emersonvisuals@gmail.com>
2022-08-20 16:37:35 +1000

    wip

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 let button = document.querySelector('.inputButton');
+let ipAddressText = document.querySelector('h1.ipAddressValue');
+let locationText = document.querySelector('h1.locationValue');
+let timezoneText = document.querySelector('h1.timezoneValue');
+let ispText = document.querySelector('h1.ispValue');
+
 
 button.addEventListener('click', () => {
-    console.log('clicked!');
+
+    function getData() {
+        fetch('https://geo.ipify.org/api/v2/country?apiKey=at_VWo7RHLulsmKoMBYuuIVzf8DcAWYK&ipAddress=8.8.8.8')
+        .then(response => response.json())
+        .then(data => {
+            ipAddressText.innerHTML = data.ip;
+            locationText.innerHTML = `${data.location.country}, ${data.location.region}`;
+            timezoneText.innerHTML = `${data.location.timezone}`;
+            ispText.innerHTML = `${data.isp}`;
+            console.log(data);
+        })
+    }
+    
+    getData();
 });
-
-function getData() {
-    fetch('https://geo.ipify.org/api/v2/country?apiKey=at_VWo7RHLulsmKoMBYuuIVzf8DcAWYK&ipAddress=8.8.8.8')
-    .then(function(result){
-        console.log(result);
-    })
-    .then(function(data){
-        console.log(data);
-    });
-}
-
-getData();

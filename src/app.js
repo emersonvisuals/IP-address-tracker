977334862de72ea60b8a43e4fee881a4f82f4344
Thomas Emerson <emersonvisuals@gmail.com>
2022-08-20 22:25:10 +1000

    wip

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-
 function firstTest() {
     fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_VWo7RHLulsmKoMBYuuIVzf8DcAWYK&ipAddress=${userInput.value}`)
     .then(response => response.json())
@@ -29,8 +28,6 @@ function firstTest() {
 }
 firstTest();
 
-
-
 button.addEventListener('click', () => {
 
     function getVal(){
@@ -44,15 +41,52 @@ button.addEventListener('click', () => {
         fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_VWo7RHLulsmKoMBYuuIVzf8DcAWYK&ipAddress=${userInput.value}`)
         .then(response => response.json())
         .then(data => {
-            ipAddressText.innerHTML = data.ip;
-            locationText.innerHTML = `${data.location.country}, ${data.location.region}`;
-            timezoneText.innerHTML = `${data.location.timezone}`;
-            ispText.innerHTML = `${data.isp}`;
-                
-            map.setView([`${data.location.lat}`, `${data.location.lng}`], `${data.location.geonameId}`);
-            L.marker([`${data.location.lat}`, `${data.location.lng}`]).addTo(map)
+            if (data.ip === undefined) {
+                alert('please enter a valid address');
+            } else {
+                ipAddressText.innerHTML = data.ip;
+                locationText.innerHTML = `${data.location.country}, ${data.location.region}`;
+                timezoneText.innerHTML = `${data.location.timezone}`;
+                ispText.innerHTML = `${data.isp}`;
+                    
+                map.setView([`${data.location.lat}`, `${data.location.lng}`], `${data.location.geonameId}`);
+                L.marker([`${data.location.lat}`, `${data.location.lng}`]).addTo(map)
+            }
         })
     }
     
     getData();
+});
+
+document.addEventListener("keyup", function(event) {
+    
+    if (event.keyCode === 13) {
+
+        function getVal(){
+            userInput.value;
+            console.log(userInput.value);  
+        }
+        
+        getVal();
+    
+        function getData() {
+            fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_VWo7RHLulsmKoMBYuuIVzf8DcAWYK&ipAddress=${userInput.value}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.ip === undefined) {
+                    alert('please enter a valid address');
+                } else {
+                    ipAddressText.innerHTML = data.ip;
+                    locationText.innerHTML = `${data.location.country}, ${data.location.region}`;
+                    timezoneText.innerHTML = `${data.location.timezone}`;
+                    ispText.innerHTML = `${data.isp}`;
+                        
+                    map.setView([`${data.location.lat}`, `${data.location.lng}`], `${data.location.geonameId}`);
+                    L.marker([`${data.location.lat}`, `${data.location.lng}`]).addTo(map)
+                }
+            })
+        }
+        
+        getData();
+    }
 });
